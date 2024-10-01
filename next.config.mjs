b2a94835bd1fefa63fdd5440ea1fe3128539d02c
Dimitri <dimitri@circleclick.com>
2024-10-01 05:46:11 -0400

    new config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // This tells Next.js to export the app as static files
-    images: {
-      unoptimized: true, // Disable image optimization for static exports
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export', 
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,  // Disable ESLint during builds
+  },
+};
+
+export default nextConfig;
